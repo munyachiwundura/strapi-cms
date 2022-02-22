d3f6a82c24cb211fcb7bc534cf06a090a010222f
munyachiwundura <munyachiwundura@gmail.com>
2022-02-22 12:06:56 +0200

    first commit

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('MY_HEROKU_URL', '0.0.0.0'),
+  // port: env.int('PORT', 1337),
+  port: env.int(process.env.PORT , 8080),
   app: {
     keys: env.array('APP_KEYS'),
   },
