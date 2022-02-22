3b4183a6eac4fc9f671f11fb3c0a7ccb593002df
munyachiwundura <munyachiwundura@gmail.com>
2022-02-22 12:37:58 +0200

    gotta work

--- config/database.js
+++ config/database.js
@@ -1,37 +1,22 @@
 const parse = require('pg-connection-string').parse;
-const path = require('path');
-
-module.exports = ({ env }) => {
-
-  if (env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password
-          },
-          options: {
-            ssl: false
-          }
-        }
-      }
-    }
-  }
-
-return {
-  defaultConnection: {
-    client: 'sqlite',
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
-}};
+});
+
+  
+  
+
